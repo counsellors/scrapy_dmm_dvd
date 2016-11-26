d417ccab93acaf61f4cd6031c1566af813d82e93
sunon <zhaoxing-5@163.com>
2016-11-26 22:26:50 +0800

    save list to mongodb

--- dmm_joke/dmm_joke/spiders/advd_list_spider.py
+++ dmm_joke/dmm_joke/spiders/advd_list_spider.py
@@ -7,35 +7,26 @@ from dmm_joke.items import ADVDDetailListItem
 from scrapy.spiders import  CrawlSpider,Rule
 from scrapy.linkextractors import LinkExtractor
 
-class AdvdListSpiderSpider(scrapy.Spider):
+class AdvdListSpiderSpider(CrawlSpider):
     name = "advd_list_spider"
-    allowed_domains = ["dmm.co.jp"]
-    start_urls = ['http://www.dmm.co.jp/digital/videoa/-/genre/']
-    start_urls = ["file:///tmp/detail_list.html"]
-    # 再一次爬去中, scrapy在rule中是自动去重的,额，省了很多事
-    # Rule(LinkExtractor(allow=('/rental/\-/list/=/.*page', ))),
-    # rules = (
-    #     Rule(LinkExtractor(allow=('/rental/\-/list/=/.*page=3', ))),
-    #     Rule(LinkExtractor(allow=('/rental/\-/detail/=/cid=\w+/', )), callback='parse_item'),
+    allowed_domains = ["dmm.com"]
+    # start_urls = ["file:///tmp/detail_list.html"]
+    start_urls = ["file:///tmp/advd_genre.html"]
+    rules = (
 
-    # )
+        Rule(LinkExtractor(allow=('digital/videoa/\-/list/=/article=keyword/id', ),
+            deny =('sort=','limit=30','limit=60','view=text',) 
+            ), callback='parse_item'),
 
-    def get_fields(self, sel,item):
-        items = []
-        for m_field in sel:
-            d_item = {}
-            d_item['link'] = m_field.xpath("div/p[2]/a/@href").extract()[0]
-            d_item['img_url'] = m_field.xpath("div/p[2]/a/span[1]/img/@src").extract()[0]
-            d_item['img_desc'] = m_field.xpath("div/p[2]/a/span[1]/img/@alt").extract()[0]
-            d_item['price'] = m_field.xpath("div/div/p[1]/text()").extract()[0]
-            items.append(d_item)
-        return items
+    )
 
-    def parse(self, response):
+    def parse_item(self, response):
         sel = Selector(response)
         items = []
+        print "lalla",response
         sel_list = sel.xpath("//*[@id='list']/li")
         for m_field in sel_list:
+            print "extract field.."
             d_item = ADVDDetailListItem()
             d_item['m_type'] = 'advd_list'
             print m_field.extract()
